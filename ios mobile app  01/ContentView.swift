5a7fb41f3842ad4c9babe010acc933072b4bd71b
thesaintzion <saintzion777@gmail.com>
2021-12-15 02:47:52 -0800

    Initial Commit

---
+++ ios mobile app  01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios mobile app  01
+//
+//  Created by mac on 15/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios mobile app  01/ios_mobile_app__01App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_mobile_app__01App.swift
+//  ios mobile app  01
+//
+//  Created by mac on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ios_mobile_app__01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
